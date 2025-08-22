60418c0196e03cc561c1afa5c42dc4cdcda223bc
NicolasGutierrez2210 <nicolas.gutierrez06@usa.edu.co>
2025-08-21 19:47:09 -0500

    sucesioneshas.hs

--- sucesioneshas.hs
+++ sucesioneshas.hs
@@ -1,10 +1,10 @@
--- Definición de la función sucesora
+-- Definición de la funcion sucesora
 sucesor :: Int -> Int
-sucesor n = n + 1   -- devuelve el siguiente número
+sucesor n = n + 1   -- devuelve el siguiente numero
 
--- Definición de la función predecesora
+-- Definición de la funcion predecesora
 predecesor :: Int -> Int
-predecesor n = n - 1   -- devuelve el número anterior
+predecesor n = n - 1   -- devuelve el numero anterior
 
 -- Suma de dos enteros positivos usando la sucesora
 suma :: Int -> Int -> Int
@@ -16,9 +16,9 @@ resta :: Int -> Int -> Int
 resta a 0 = a   -- caso base: restar 0 no cambia el valor
 resta a b = resta (predecesor a) (predecesor b)   -- reduce ambos hasta que b sea 0
 
--- Multiplicación de dos enteros positivos usando sumas repetidas
+-- Multiplicacion de dos enteros positivos usando sumas repetidas
 multiplicacion :: Int -> Int -> Int
-multiplicacion _ 0 = 0   -- caso base: todo número por 0 es 0
+multiplicacion _ 0 = 0   -- caso base: todo numero por 0 es 0
 multiplicacion a b = suma a (multiplicacion a (predecesor b))   -- suma a, b veces
 
 -- División de dos enteros positivos usando restas sucesivas
@@ -26,7 +26,7 @@ division :: Int -> Int -> Int
 division _ 0 = error "No se puede dividir entre 0"   -- controla división entre cero
 division a b
     | a < b     = 0   -- si a es menor que b, el cociente es 0
-    | otherwise = sucesor (division (resta a b) b)   -- cuenta cuántas veces se puede restar b de a
+    | otherwise = sucesor (division (resta a b) b)   -- cuenta cuantas veces se puede restar b de a
 
 -- Suma de números reales
 addReal :: Float -> Float -> Float
@@ -41,3 +41,4 @@ main = do
     print (division 10 2)         -- resultado: 5
     print (division 9 2)          -- resultado: 4
     print (addReal 5.5 3.2)       -- resultado: 8.7
+
